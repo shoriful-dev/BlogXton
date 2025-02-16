087ea8389d5ab13657b408c193d72805a3882b91
shoriful-dev <cmshohelrana50@gmail.com>
2025-02-16 14:41:12 +0600

    some change error part

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -5,7 +5,6 @@ import Image from "../components/Image";
 import ErrorImage from "../assets/pngwingcom.png";
 import { FaHome } from "react-icons/fa";
 
-
 const Error: React.FC = () => {
   return (
     <div className="bg-ErrorBG bg-[#fffaf0] h-[750px] py-">
